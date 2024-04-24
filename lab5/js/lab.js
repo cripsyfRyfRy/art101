e9e8b08bb230f6b467e3f95bfea3c1fcd9af22c4
cripsyfRyfRy <thomashollenbach2@gmail.com>
2024-04-24 12:57:52 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,24 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Ryan Hollenbach
+// Date: April 24, 2024
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//calculate
+age = 2024 - year;
 
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
