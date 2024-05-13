7f4d81a2541fd847f2dd964c00d566d60329afa6
cripsyfRyfRy <thomashollenbach2@gmail.com>
2024-05-12 17:05:21 -0700

    lab 9 update

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,41 +6,46 @@
 
 // Functions
 
-// add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+// // add button to challenge section
+// $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$(".minor-section").append("<button class='button-special'>Make Special</button>");
 
-// add a click listener to the challenge button
-$("#button-challenge").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#challenge").toggleClass("special");
+$(".button-special").click(function(){
+  $(this).parent().toggleClass("special");
 });
 
-// add button to problems section
-$("#problems").append("<button id='button-problems'>Make Special</button>");
+// // add a click listener to the challenge button
+// $("#button-challenge").click(function(){
+//   // now add (or subtract) the "special" class to the section
+//   $("#challenge").toggleClass("special");
+// });
 
-// add a click listener to the problems button
-$("#button-problems").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#problems").toggleClass("special");
-});
+// // add button to problems section
+// $("#problems").append("<button id='button-problems'>Make Special</button>");
 
-// add button to reflection section
-$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+// // add a click listener to the problems button
+// $("#button-problems").click(function(){
+//   // now add (or subtract) the "special" class to the section
+//   $("#problems").toggleClass("special");
+// });
 
-// add a click listener to the reflection button
-$("#button-reflection").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#reflection").toggleClass("special");
-});
+// // add button to reflection section
+// $("#reflection").append("<button id='button-reflection'>Make Special</button>");
 
-// add button to results section
-$("#results").append("<button id='button-results'>Make Special</button>");
+// // add a click listener to the reflection button
+// $("#button-reflection").click(function(){
+//   // now add (or subtract) the "special" class to the section
+//   $("#reflection").toggleClass("special");
+// });
 
-// add a click listener to the results button
-$("#button-results").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#results").toggleClass("special");
-});
+// // add button to results section
+// $("#results").append("<button id='button-results'>Make Special</button>");
+
+// // add a click listener to the results button
+// $("#button-results").click(function(){
+//   // now add (or subtract) the "special" class to the section
+//   $("#results").toggleClass("special");
+// });
 
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
